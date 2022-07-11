8216b6e84d3cb175a6394924408026bd21f5ddc2
Bogdan Dobrelya <bogdando@yahoo.com>
2022-07-11 10:18:06 +0200

    Poke aws provider version
    Signed-off-by: Bogdan Dobrelya <bogdando@yahoo.com>

--- modules/aws/versions.tf
+++ modules/aws/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
-    aws        = ">= 3.72"
+    aws        = ">= 4.22.0"
     helm       = "~> 2.0"
     kubernetes = "~> 2.0, != 2.12"
     kubectl = {
