dfc04146103bb8b0c72296e62e61b024c17c2da6
Bogdan Dobrelia <bogdan.dobrelia@spearline.com>
2023-06-13 10:15:40 +0200

    Add pipeline helper script for cherry-picking
    Compare commits in the given dev and prod branches, omit
    commit messages that present in both places.
    
    Signed-off-by: Bogdan Dobrelia <bogdan.dobrelia@spearline.com>

---
+++ pipeline.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# A helper script to get commits delta for cherry-picking to prod from dev
+PRODBR=${PRODBR:-origin/prod}
+DEVBR=${DEVBR:-origin/main}
+
+DELTA="${PRODBR}..${DEVBR}"
+LOGPRODMSG="git --no-pager log --no-merges --pretty=format:'%s' ${PRODBR}; echo"
+LOGDEVMSG="git --no-pager log --no-merges --pretty=format:'%s' ${DELTA}; echo"
+LOGDEVALL="git --no-pager log --no-merges --pretty=format:'%h - %an, %ar : %s' ${DELTA}; echo"
+
+TMPDIR="$(mktemp -d)"
+trap 'rm -f ${TMPDIR}/gitlog*; rm -d "$TMPDIR"' EXIT
+
+eval $LOGPRODMSG > ${TMPDIR}/gitlogprod
+eval $LOGDEVMSG > ${TMPDIR}/gitlog
+eval $LOGDEVALL > ${TMPDIR}/gitlogall
+
+# Omit commit messages from dev what already present in prod
+while read msg; do
+	[ -z "$msg" ] && break
+	if grep -q "$msg" ${TMPDIR}/gitlogprod ; then
+    grep -v "$msg" ${TMPDIR}/gitlogall > ${TMPDIR}/gitlogall_
+  	mv -f ${TMPDIR}/gitlogall_ ${TMPDIR}/gitlogall
+  fi
+done < "${TMPDIR}/gitlog"
+
+cat ${TMPDIR}/gitlogall
